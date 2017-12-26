0fbcda2a489a507f4dda641b4ce7bea5c5c2b68e
Suhavi Sandhu <sandhs11@mcmaster.ca>
2017-12-25 22:56:12 -0500

    added scroll text on top

--- WinterBreak/public/stocks.js
+++ WinterBreak/public/stocks.js
@@ -5,7 +5,8 @@ var socket = io.connect('http://localhost:4000');
 // https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_INTRADAY&symbol=BTC&market=CAD&apikey=P1IPN8JROHA6T8KB
 // when someone clicks BUYY this will do something
 var players = document.getElementById('players'); // output
-
+var money = document.getElementById('money');
+var scroll = document.getElementById('scroll');
 
 // listen for events, when the backend finds out that click occured, frontend posts number
 socket.on('playerson', function(data){
@@ -13,6 +14,39 @@ socket.on('playerson', function(data){
 });
 
 
-$(function(){
-    console.log('hello');
-})
+socket.on('connect', function(){
+    
+    // Make some API requests
+    data = [
+        {
+            name: 'BitCoin',
+            symbol: 'BTC',
+            price: '1',
+            shares: '0'
+        },
+
+        {
+            name: 'LiteCoin',
+            symbol: 'LTC',
+            price: '2',
+            shares: '0'
+        },
+
+        {
+            name: 'Ethereum',
+            symbol: 'ETM',
+            price: '3',
+            shares: '0'
+        }
+    ];
+    var total = 1000;    
+    money.innerHTML = '<h1> Money: '+total+'</h1>';
+    var scroll_text = '';
+    for (i = 0; i < data.length; i++){
+        scroll_text += data[i].symbol + ' ' + data[i].price + ' | '
+
+    }
+
+    scroll.innerHTML = '<marquee scrollamount="20"><h1>'+scroll_text+'</h1></marquee>';
+    
+});
