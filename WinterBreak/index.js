1fc74535b44339297bddb36ec564f8d28d164fe9
Suhavi Sandhu <sandhs11@mcmaster.ca>
2017-12-26 23:34:08 -0500

    made http request to cryptocompare

--- WinterBreak/index.js
+++ WinterBreak/index.js
@@ -1,6 +1,8 @@
 // this file is for server-side, index.html is for client side
 var express = require('express');
 var socket = require('socket.io');
+var https = require('https');
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 // Set up the app
 var app = express();
@@ -16,6 +18,47 @@ app.use(express.static('public'));
 // socket set up, parameter = server we want to work with
 var io = socket(server);
 var players = 0;
+var from = 'BTC,ETH,LTC,XRP,DASH,XMR,OMG,DOGE,NXT,ZEC';
+var to = 'USD';
+
+// Supply the data for prices, change in currency, 
+https.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms='+from+'&tsyms=USD', (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
+  
+    let error;
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+                        `Status Code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+      error = new Error('Invalid content-type.\n' +
+                        `Expected application/json but received ${contentType}`);
+    }
+    if (error) {
+      console.error(error.message);
+      // consume response data to free up memory
+      res.resume();
+      return;
+    }
+  
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        console.log(parsedData);
+        var info = parsedData;
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  }).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+  });
+
+  
+
 // when connection is made, function is called
 // index.html -> runs stocks.js -> makes connection 
 io.on('connection', function(socket) {
@@ -24,6 +67,15 @@ io.on('connection', function(socket) {
     io.sockets.emit('playerson', {
         players: ++players
     });
+    var prices = [0,1,2,3,4,5,6,7,8,9];
+    var changes = [0,1,2,3,4,5,6,7,8,9];
+    
+    io.sockets.emit('update', {
+      prices: prices,
+      changes: changes
+      });
+      console.log(prices[1]);
+    
     socket.on('disconnect', function(){
         io.sockets.emit('playerson', {
             players: --players
--- WinterBreak/public/stocks.js
+++ WinterBreak/public/stocks.js
@@ -1,54 +1,68 @@
 // Make connection for client-side
 var socket = io.connect('http://localhost:4000');
 
+
 //  P1IPN8JROHA6T8KB Alpha Vantage Key
 // https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_INTRADAY&symbol=BTC&market=CAD&apikey=P1IPN8JROHA6T8KB
 // when someone clicks BUYY this will do something
 var players = document.getElementById('players'); // output
 var money = document.getElementById('money');
 var scroll = document.getElementById('scroll');
+var cryptodata = [];
+var prices = [];
+var changes = [];
+var coins = [
+    ['BTC', 'Bitcoin'], 
+    ['ETH', 'Ethereum'],
+    ['LTC', 'Litecoin'],
+    ['XRP', 'Ripple'],
+    ['DASH', 'Dash'],
+    ['XMR', 'Monero'],
+    ['OMG', 'OmiseGO'],
+    ['DOGE', 'Dogecoin'],
+    ['NXT', 'Nxt'],
+    ['ZEC', 'ZCash'],
+];
 
 // listen for events, when the backend finds out that click occured, frontend posts number
 socket.on('playerson', function(data){
     players.innerHTML = '<p>Players online: '+data.players+'</p>';
 });
 
+socket.on('update', function(data){
+    prices = data.prices;
+    changes = data.changes;
+    for (var i in coins){
+        cryptodata.push({
+            name: coins[i][1],
+            symbol: coins[i][0],
+            price: prices[i],
+            change: changes[i],
+            shares: 0
+        });
+    };
+    var scroll_text = '';
+    for (i = 0; i < cryptodata.length; i++){
+        scroll_text += cryptodata[i].symbol + ' ' + cryptodata[i].price + ' | '
+    };
+    scroll.innerHTML = '<marquee scrollamount="20"><h1>'+scroll_text+'</h1></marquee>'
+});
 
 socket.on('connect', function(){
     
-    // Make some API requests
-    
-   var coins = [
-       ['BTC', 'Bitcoin'], 
-       ['ETH', 'Ethereum'],
-       ['LTC', 'Litecoin'],
-       ['XRP', 'Ripple'],
-       ['DASH', 'Dash'],
-       ['XMR', 'Monero'],
-       ['OMG', 'OmiseGO'],
-       ['DOGE', 'Dogecoin'],
-       ['NXT', 'Nxt'],
-       ['ZEC', 'ZCash'],
-   ];
-
-   var data = []
    // create data
-   for (var i in coins){
-       data.push({
-           name: coins[i][1],
-           symbol: coins[i][0],
-           price: 1,
-           shares: 0
-       });
-   };
+   
+
+   
 
     var total = 1000;    
     money.innerHTML = '<h1> Money: '+total+'</h1>';
-    var scroll_text = '';
-    for (i = 0; i < data.length; i++){
-        scroll_text += data[i].symbol + ' ' + data[i].price + ' | '
-    }
+    
 
-    scroll.innerHTML = '<marquee scrollamount="20"><h1>'+scroll_text+'</h1></marquee>';
+    
+    scroll.innerHTML = '<marquee scrollamount="20"><h1>'+prices[1]+'</h1></marquee>';
+        // Make some API requests
+
+   
     
 });
